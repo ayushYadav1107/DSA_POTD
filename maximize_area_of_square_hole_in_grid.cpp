cbfdc9ac28a24826cfa1aa9e0773f3e97e5749c6
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-01-16 19:57:37 +0530

    confusing problem statement

--- maximize_area_of_square_hole_in_grid.cpp
+++ maximize_area_of_square_hole_in_grid.cpp
@@ -4,17 +4,32 @@ public:
         sort(hBars.begin(), hBars.end());
         sort(vBars.begin(), vBars.end());
 
-        auto maxSpan = [](vector<int>& bars) {
-            int res = 1, streak = 1;
-            for (int i = 1; i < bars.size(); i++) {
-                if (bars[i] - bars[i - 1] == 1) streak++;
-                else streak = 1;
-                res = max(res, streak);
-            }
-            return ++res;
-        };
+        int maxHBars = 1;
+        int maxVBars = 1;
 
-        int s = min(maxSpan(hBars), maxSpan(vBars));
-        return s * s;
+        // longest length of consecutive H bars
+        int currMaxHBars = 1;
+        for(int i = 0; i < hBars.size()-1;i++){
+            if((hBars[i+1] - hBars[i])==1){
+                currMaxHBars++;
+            }else{
+                currMaxHBars = 1;
+            }
+            maxHBars = max(maxHBars,currMaxHBars);
+        } 
+        // similarly length of consecutive V bars
+        int currMaxVBars = 1;
+        for(int i = 0; i < vBars.size()-1;i++){
+            if((vBars[i+1] - vBars[i])==1){
+                currMaxVBars++;
+            }else{
+                currMaxVBars = 1;
+            }
+            maxVBars = max(maxVBars,currMaxVBars);
+        }
+
+        // finding the minimum of the two to ensure its a square
+        int sqSize = min(maxVBars,maxHBars) + 1;
+        return sqSize * sqSize;
     }
 };
