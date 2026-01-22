442a65f754db9cc0ac7ec755d5c284c7bf4fcfbb
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-01-22 23:26:13 +0530

    brute force works due to small constraint

---
+++ minimum_pair_removal_to_sort_array_I.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int minPairSum(vector<int>&nums){
+        int minSum = INT_MAX;
+        int index = 0;
+        for(int i = 0; i<nums.size()-1;i++){
+            int sum = nums[i] + nums[i+1];
+            if(sum < minSum){
+                minSum = sum;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        int operations = 0;
+        while(!is_sorted(nums.begin(),nums.end())){
+            int index = minPairSum(nums);
+
+            nums[index] = nums[index] + nums[index+1];
+
+            nums.erase(nums.begin() + (index+1));
+
+            operations++;
+        }
+        return operations;
+    }
+};
