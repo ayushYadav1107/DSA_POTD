f1cdddcdcefbfc941ddbcdbbabd083d207e90366
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-02-07 22:32:30 +0530

    greedy approach

---
+++ minimum_deletions_to_make_string_balanced.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+
+        int deletions = 0;
+        int bCount = 0;
+        for(int i = 0; i < n; i++){
+            if(s[i]=='b'){
+                bCount++;
+            }else{
+                if(bCount > 0){
+                    deletions++;
+                    bCount--;
+                }
+            }
+        }
+        return deletions;
+    }
+};
