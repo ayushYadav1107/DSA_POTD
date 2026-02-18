b7e764c9e1acdca4f87a5885e5de8bd958b84b84
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-02-18 22:29:38 +0530

    using bitset and division method

--- binary_no_with_alternating_bits.cpp
+++ binary_no_with_alternating_bits.cpp
@@ -14,3 +14,21 @@ public:
         return true;
     }
 };
+
+// method -2 => using binary division
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prevBit = n % 2;
+        n /= 2;
+        while(n > 0){
+            int currBit = n % 2;
+            if(currBit == prevBit){
+                return false;
+            }
+            n /= 2;
+            prevBit = currBit;
+        }
+        return true;
+    }
+};
