b0412b311d7c63d5e105f3c30bb6a211b3653b84
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-02-18 22:03:29 +0530

    bitset method

---
+++ binary_no_with_alternating_bits.cpp
@@ -0,0 +1,16 @@
+
+// Method-1) => bitset method
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        bitset<32> b(n);
+        int highestSetBit = log2(n);
+
+        for(int i = 0; i < highestSetBit;i++){
+            if(b[i]==b[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
