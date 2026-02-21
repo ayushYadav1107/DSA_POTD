3ba083c8d4f8b9fa89a5b4dda95db3f9bd016e41
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-02-21 19:29:38 +0530

    using STL to find set bits

---
+++ prime_number_of_set_bits_in_binary_representation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int>st{2,3,5,7,11,13,17,19,23,29,31};
+        int count = 0;
+        for(int i = left; i <=right;i++){
+            int setBits = __builtin_popcount(i);
+            if(st.find(setBits) != st.end()){
+                count++;
+            }
+        }
+        return count;
+    }
+};
