068dedf2b8eb95ddf1787d7b7079710bb034e0b9
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-02-22 17:17:04 +0530

    added new methods

--- binary_gap.cpp
+++ binary_gap.cpp
@@ -24,3 +24,42 @@ public:
         return maxDistance;
     }
 };
+
+// method -2 TC => O(logn)
+class Solution {
+public:
+    int binaryGap(int n) {
+        int curr = 0;
+        int prev = -1;
+
+        int result = 0;
+
+        while(n > 0){
+            if((n&1)>0){
+                result = (prev != -1) ? max(result,curr-prev): result;
+                prev = curr;
+            }
+            curr++;
+            n >>= 1;
+        }
+        return result;
+    }
+};
+
+//method-3 using for Loop
+class Solution {
+public:
+    int binaryGap(int n) {
+        int prev = -1;
+
+        int result = 0;
+
+        for(int curr = 0; curr < 32; curr++){
+            if(((n >> curr)&1) == 1){
+                result = (prev != -1) ? max(result,curr-prev): result;
+                prev = curr;
+            }
+        }
+        return result;
+    }
+};
