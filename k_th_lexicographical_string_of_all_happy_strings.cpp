4528d0d3c4f91832ae0a6d0c5fe79885b3052ca0
ayushYadav1107 <himmatyadav1995@gmail.com>
2026-03-16 00:03:04 +0530

    good question used a lot of math knowledge

--- k_th_lexicographical_string_of_all_happy_strings.cpp
+++ k_th_lexicographical_string_of_all_happy_strings.cpp
@@ -22,3 +22,38 @@ public:
         return result[k-1];
     }
 };
+
+TC : (n * 2^n);
+SC : same 
+
+// method -2 optimized :
+class Solution {
+public:
+    vector<char>vec{'a','b','c'};
+    void solve(string &curr, string &result,int n,int &count,int &k){
+        if(curr.length()==n){
+            count++;
+            if(count==k){
+                result = curr; // we got our happy string
+            }
+            return;
+        }
+
+        for(auto &ch : vec){
+            if( !curr.empty() && curr.back() == ch) continue;
+            curr.push_back(ch); // do
+            solve(curr,result,n,count,k); // explore
+            curr.pop_back(); // undo
+        }
+    }
+    string getHappyString(int n, int k) {
+        string result = "";
+        string curr = "";
+        int count = 0;
+        solve(curr,result,n,count,k);
+        return result;
+    }
+};
+
+TC : (n * 2^n)
+SC : (n) where n is the depth of tree diagram
